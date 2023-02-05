59b5b112b18203875d69b419f6a0b7e0374edbd1
Chanaka Subasingha <chanaka.s@icloud.com>
2023-02-05 22:44:55 +0100

    modified

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,9 +1,6 @@
-import { BadRequestException, Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { AuthGuard } from '@nestjs/passport';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { ROLE } from 'src/common/enums';
 import { RolesGuard } from 'src/auth/guards/role.guard';
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,17 +1,12 @@
 import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateUserDto } from './dto/create-user.dto';
 import { User, UserDocument } from './schemas/user.schema';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
     constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) { }
-
-    async create(createUserDto: CreateUserDto): Promise<User> {
-        return await this.userModel.create(createUserDto);
-    }
 
     async findAll(): Promise<User[]> {
         return this.userModel.find().exec();
