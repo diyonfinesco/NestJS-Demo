17423e62743681848531557467cbf7e1ab7f8e4c
Chanaka Subasingha <chanaka.s@icloud.com>
2023-02-06 22:17:37 +0100

    fixed the bug

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -3,10 +3,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from 'src/auth/dto/CreateUserDto';
 
 @Injectable()
 export class UserService {
     constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>) { }
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        return await this.userModel.create(createUserDto);
+    }
 
     async findAll(): Promise<User[]> {
         return this.userModel.find().exec();
